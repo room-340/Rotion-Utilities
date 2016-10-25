fad7c166dd226b896dc7af9091778f01777ea318
pec-orange <pec-orange@mail.ru>
2016-10-25 16:57:49 +0300

    Долой ненужные команды!
    Ещё поменял направление вращения (conjQ), для визуального соответствия вращению объекта.

--- quatAnimation.m
+++ quatAnimation.m
@@ -2,10 +2,11 @@ function quatAnimation(quaternion)
     % INPUT:
     % quaternion - quaternions array [length x 4], in [w i j k] order
     baseVector = [0.5774 0.5774 0.5774];
+%     baseVector = [0 0 1];
     basePosition = [0 0 0];
-    pointerVector = [0 0.2 0];
+%     pointerVector = [0 0.2 0];
     len = length(quaternion);
-    skipper = round(len/1000);
+    skipper = round(len/750);
 %     skipper = 1;
     figure
     grid on
@@ -33,22 +34,24 @@ function quatAnimation(quaternion)
     line([-1 1], [-1 1], [-1 1], 'LineWidth', 1, 'LineStyle','--', 'Color', [0.5, 0.5, 0.5]);
     line([-1 1], [1 -1], [-1 1], 'LineWidth', 1, 'LineStyle','--', 'Color', [0.5, 0.5, 0.5]);
     
-    pic = line([basePosition(1) baseVector(1)],[basePosition(2) baseVector(2)],[basePosition(3) baseVector(3)], 'LineWidth', 2, 'Color', 'k');
-    pVec = baseVector + pointerVector;
+    pVec = basePosition + baseVector;
+    pic = line([basePosition(1) pVec(1)],[basePosition(2) pVec(2)],[basePosition(3) pVec(3)], 'LineWidth', 2, 'Color', 'k');
+    
 %     ptr = line([baseVector(1) pVec(1)],[baseVector(2) pVec(2)],[baseVector(3) pVec(3)], 'LineWidth', 2, 'Color', 'k');
 %     pause
     prevPos = pVec;
     for k=1:skipper:len
-        pause(0.01);
+%         pause(0.01);
         delete(pic);
 %         delete(ptr);
-        vector = rotateVQ(baseVector, quaternion(k,:));
-        pVec = rotateVQ(pointerVector, quaternion(k,:));
-        pVec = vector + pVec;
+        vector = quatrotate(conjq(quaternion(k,:)), baseVector);
+%         pVec = quatrotate(quaternion(k,:), pointerVector);
+%         pVec = vector + pVec;
+        vector = basePosition + vector;
         pic = line([basePosition(1) vector(1)],[basePosition(2) vector(2)],[basePosition(3) vector(3)], 'LineWidth', 2, 'Color', 'k');
 %         ptr = line([vector(1) pVec(1)],[vector(2) pVec(2)],[vector(3) pVec(3)], 'LineWidth', 2, 'Color', 'k');
         pVec = vector;
-        dotVec = pVec + 0.02;
+%         dotVec = pVec + 0.02;
 %         line([pVec(1) dotVec(1)],[pVec(2) dotVec(2)],[pVec(3) dotVec(3)], 'LineWidth', 2, 'Color', 'k');
         line([prevPos(1) pVec(1)],[prevPos(2) pVec(2)],[prevPos(3) pVec(3)], 'LineWidth', 2, 'Color', [0.8,0.4,0]);
         prevPos = pVec;
